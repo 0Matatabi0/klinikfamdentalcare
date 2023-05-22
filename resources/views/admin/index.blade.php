39fe2b587c66b1a1cb577bd6a9fd13c40e0698a5
Desita Nurrohmah <130421663+daissagi@users.noreply.github.com>
2023-05-22 23:15:04 +0700

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,8 +16,7 @@
                     </div>
                     <div class="col-sm-5 text-center text-sm-left">
                         <div class="card-body pb-0 px-0 px-md-4">
-                            <img src="{{ asset('admin/img/illustrations/man-with-laptop-light.png') }}" height="140"
-                                alt="View Badge User"/>
+                            <img src="{{ asset('admin/img/illustrations/man-with-laptop-light.png') }}" height="140" alt="View Badge User" />
                         </div>
                     </div>
                 </div>
@@ -75,7 +74,7 @@
                                 </div>
                             </div>
                         </li>
-                        <li class="d-flex mb-4 pb-1">
+                        <li class="d-flex mb-4 pb-2">
                             <div class="avatar flex-shrink-0 me-3">
                                 <span class="bx bx-images"></span>
                             </div>
